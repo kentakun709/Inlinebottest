41966128e0d8c49f6319f7830e2f848d06644338
Kenta Inakazu <kenta.inak@gmail.com>
2022-05-19 11:40:43 +0900

    変更テスト

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage("aaaWorld"))
+        TextSendMessage("aaaWorldnanodaaaa"))
 
 
 if __name__ == "__main__":
