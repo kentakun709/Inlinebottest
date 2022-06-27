76771797870e66a8ef0cb65018de8c9c802f1a18
西辻 怜央 <leonstj3@iCloud.com>
2022-06-27 13:04:14 +0900

    test

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 LINE_BOT_API = Settings.LINE_BOT_API
 LINE_BOT_HANDLER = Settings.LINE_BOT_HANDLER
 
-# 一斉送信権限グループ
+# 一斉送信権限グループ  
 FORCE_GROUP_ID = Settings.FORCE_GROUP_ID
 
 @app.route("/")
